f336be6b3c1bd7a18ba2725489b6ef53d9e13066
ROKAISAKKON <rokaisakkon@gmail.com>
2016-05-05 22:28:11 +0700

    add lang Thai

---
+++ src/Lang/th/messages.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ * Forum translation file.
+ *
+ */
+return [
+    'forum'                    => 'เว็บบอร์ด',
+    'general'                  => 'ทั่วไป',
+    'new-conversation'         => 'โพสต์',
+    'log-in'                   => 'เข้าสู่ระบบ',
+    'search'                   => 'ค้นหา',
+    'all'                      => 'ทั้งหมด',
+    'no-conversations'         => 'เราไม่พบสิ่งที่คุณค้นหา',
+    'replies-counter'          => ':replies replies in this conversation',
+    'replies'                  => 'ตอบกลับ|ตอบกลับ',
+    'post'                     => 'โพสต์ตอบกลับของคุณ',
+    'post-conversation'        => 'โพสต์',
+    'cancel'                   => 'ยกเลิก',
+    'text-like'                => 'Like',
+    'title'                    => 'ชื่อเรื่อง?',
+    'topic'                    => 'เลือกบอร์ด',
+    'message'                  => 'เนื้อหา',
+    'notification-reply'       => 'มีคำตอบใหม่ในคำถามของคุณ',
+    'best-answer-asker-choise' => 'คำตอบที่ดีที่สุด',
+    'commonmark-support'       => 'ได้รับการสนับสนุน',
+];
